304db45439b02111967b7cd072faf59bf6319c31
AwETux <lincraftserver@gmail.com>
2024-11-18 08:58:13 -0600

    v

--- build.sh
+++ build.sh
@@ -1,31 +1,31 @@
 echo "Building..."
 
-echo "" > ./viper.src
+echo "" > ./dddd.src
 
 for i in $(ls ./libs);
 do
-    cat ./libs/$i >> ./viper.src
-    echo "" >> ./viper.src
+    cat ./libs/$i >> ./dddd.src
+    echo "" >> ./dddd.src
 done
 
 for i in $(ls ./functions);
 do
-    cat ./functions/$i >> ./viper.src
-    echo "" >> ./viper.src
+    cat ./functions/$i >> ./dddd.src
+    echo "" >> ./dddd.src
 done
 
 for i in $(ls ./core_commands);
 do
-    cat ./core_commands/$i >> ./viper.src
-    echo "" >> ./viper.src
+    cat ./core_commands/$i >> ./dddd.src
+    echo "" >> ./dddd.src
 done
 
 for i in $(ls ./commands);
 do
-    cat ./commands/$i >> ./viper.src
-    echo "" >> ./viper.src
+    cat ./commands/$i >> ./dddd.src
+    echo "" >> ./dddd.src
 done
 
-cat ./main/main.src >> ./viper.src
+cat ./main/main.src >> ./dddd.src
 
 echo "Done building!"
